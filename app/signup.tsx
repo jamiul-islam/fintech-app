df693cb3d653114dfc07ad38a9905c173fbffb08
jamiul-islam <jamiulislamjami@hotmail.com>
2024-10-15 13:12:46 +0600

    welcomeScreen done

---
+++ app/signup.tsx
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+
+const Page = () => {
+  const onSignUp = async () => {
+    // TODO: clerk authentication
+  };
+
+  return (
+    <View style={defaultStyles.container}>
+      <Text style={defaultStyles.header}>Let's get started!</Text>
+      <Text style={defaultStyles.descriptionText}>
+        Enter your phone number. We will send you a confirmation code there
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: "row",
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
+
+export default Page;

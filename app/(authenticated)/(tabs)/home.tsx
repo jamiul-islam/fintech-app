2fde1baac3ffad87668fe5a6bdf2e7e16c29ee93
jamiul-islam <jamiulislamjami@hotmail.com>
2024-10-18 12:27:59 +0600

    drag and drop gesture initialized

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,10 +1,19 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import Colors from "@/constants/Colors";
 import { useHeaderHeight } from "@react-navigation/elements";
 import RoundBtn from "@/components/RoundBtn";
 import Dropdown from "@/components/Dropdown";
 import { useBalanceStore } from "@/store/balanceStore";
+import { defaultStyles } from "@/constants/Styles";
+import { Ionicons } from "@expo/vector-icons";
+import WidgetList from "@/components/SortableList/WidgetList";
 
 const Page = () => {
   const { balance, runTransaction, transactions, clearTransactions } =
@@ -12,7 +21,14 @@ const Page = () => {
 
   const headerHeight = useHeaderHeight();
 
-  const onAddMoney = () => {};
+  const onAddMoney = () => {
+    runTransaction({
+      id: Math.random().toString(),
+      title: "Added money",
+      amount: Math.floor(Math.random() * 1000) * (Math.random() > 0.5 ? 1 : -1),
+      date: new Date(),
+    });
+  };
 
   return (
     <ScrollView
@@ -26,6 +42,17 @@ const Page = () => {
           <Text style={styles.balance}>{balance()}</Text>
           <Text style={styles.currency}>€</Text>
         </View>
+
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButtonSmall,
+            { backgroundColor: Colors.lightGray, marginVertical: 20 },
+          ]}
+        >
+          <Text style={[defaultStyles.buttonTextSmall, { color: Colors.dark }]}>
+            Accounts
+          </Text>
+        </TouchableOpacity>
       </View>
 
       <View style={styles.actionRow}>
@@ -38,6 +65,40 @@ const Page = () => {
         <RoundBtn icon={"list"} text={"Details"} />
         <Dropdown />
       </View>
+
+      <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+      <View style={styles.transactions}>
+        {transactions.length === 0 && (
+          <Text style={{ padding: 14, color: Colors.gray }}>
+            No transactions yet
+          </Text>
+        )}
+        {transactions.map((transaction) => (
+          <View
+            key={transaction.id}
+            style={{ flexDirection: "row", alignItems: "center", gap: 16 }}
+          >
+            <View style={styles.circle}>
+              <Ionicons
+                name={transaction.amount > 0 ? "add" : "remove"}
+                size={24}
+                color={Colors.dark}
+              />
+            </View>
+
+            <View style={{ flex: 1 }}>
+              <Text style={{ fontWeight: "400" }}>{transaction.title}</Text>
+              <Text style={{ color: Colors.gray, fontSize: 12 }}>
+                {transaction.date.toLocaleString()}
+              </Text>
+            </View>
+            <Text>{transaction.amount}€</Text>
+          </View>
+        ))}
+      </View>
+
+      <Text style={defaultStyles.sectionHeader}>Widgets</Text>
+      <WidgetList />
     </ScrollView>
   );
 };
